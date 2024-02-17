edb3120fa05eb0ffce4f1e26978a35aa6503c048
Joe Hildebrand <joe-github@cursive.net>
2024-02-17 06:28:15 -0700

    First checkin

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = [
+  {
+    ignores: [
+      "node_modules/**",
+    ],
+  },
+  require("@peggyjs/eslint-config/flat/js"),
+  require("@peggyjs/eslint-config/flat/modern"),
+];
